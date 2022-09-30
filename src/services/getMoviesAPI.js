6091da237024c6aca355847415b3c3b73402fa97
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-09-30 11:09:30 +0100

    ceates function fetchCast

--- src/services/getMoviesAPI.js
+++ src/services/getMoviesAPI.js
@@ -29,14 +29,27 @@ function fetchTrending() {
 function fetchMovieDetalis(movieId) {
 
     return (
-    fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
+        fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
 
-        return Promise.reject(new Error('The API_KEY is wrong'))
-    })
-)
+            return Promise.reject(new Error('The API_KEY is wrong'))
+        })
+    );
+};
 
-}
+function fetchCast(movieId) {
+
+    return (
+        fetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+
+            return Promise.reject(new Error('The API_KEY is wrong'))
+        })
+    );
+};
